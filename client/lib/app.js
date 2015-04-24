f1f2ddb1da29154c45dfee269dca0550a23258e2
ErikMellum <emellum@mail.csuchico.edu>
2015-04-23 20:57:27 -0700

    Fixes typo

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,1 +1,10 @@
 angular.module('cloudnote',['angular-meteor', 'ui.router']);
+
+function onReady() {
+  angular.bootstrap(document, ['cloudnote']);
+}
+
+if (Meteor.isCordova)
+  angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
