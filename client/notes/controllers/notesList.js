ae4b96b05361b9c92ba212113da8f0de7f1c2864
ErikMellum <emellum@mail.csuchico.edu>
2015-05-05 17:06:12 -0700

    Umm Yeah

--- client/notes/controllers/notesList.js
+++ client/notes/controllers/notesList.js
@@ -1,12 +1,16 @@
-angular.module("cloudnote").controller("NoteListController", ['$scope', '$meteor', '$rootScope',
-    function($scope, $meteor, $rootScope){
+angular.module("cloudnote").controller("NoteListController", ['$scope', '$meteor', '$rootScope', '$log',
+    function($scope, $meteor, $rootScope, $log){
 
       $meteor.subscribe('users');
-
+      $scope.noteFormVisible = false;
       $scope.page = 1;
       $scope.perPage = 3;
       $scope.sort = { name: 1 };
       $scope.orderProperty = '1';
+      $scope.noteContent = '';
+      $scope.noteVisible=false;
+      $scope.smallScreen;
+      $scope.noteListVisible=true;
 
       $scope.notes = $meteor.collection(function() {
         return Notes.find({}, {
@@ -29,6 +33,65 @@ angular.module("cloudnote").controller("NoteListController", ['$scope', '$meteor
         if ($scope.orderProperty)
           $scope.sort = {name: parseInt($scope.orderProperty)};
       });
+
+      $scope.submitNote = function() {
+        $scope.newNote.owner=$root.currentUser._id; 
+        $scope.notes.push(newNote); 
+        $scope.showNoteForm=false;
+      }
+
+      $scope.setContents = function(contents) {
+        $scope.noteContent=contents;
+        $scope.noteVisible=true;
+        $scope.hideNoteList();
+      }
+
+      $scope.showNote = function() {
+        if($scope.noteVisible)
+          return true;
+        return false;
+      }
+
+
+
+      $scope.hideNoteList = function () {
+        if($scope.smallScreen && $scope.noteVisible){
+          $scope.noteListVisible= false;
+          console.log("Note List Should be Hiding");
+        }
+        else {
+          console.log("Note List Should be Visible");
+          $scope.noteListVisible= true;
+        }
+      }
+
+      $scope.checkWindowSize = function() {
+        if ( $(window).innerWidth() < 768 ) {
+          $scope.smallScreen = true;
+          console.log("Window size is small");
+          $scope.hideNoteList();
+        }
+        else {
+          $scope.smallScreen = false;
+          console.log("Window size is not small");
+          $scope.hideNoteList();
+        }
+      }
+      $(window).load($scope.checkWindowSize);
+      $(window).resize($scope.checkWindowSize);
+
+      $scope.hideNote = function() {
+        $scope.noteVisible=false;
+        $scope.hideNoteList();
+      }
+
+      $scope.showNoteForm = function() {
+        $scope.noteFormVisible=true;
+      };
+
+      $scope.hideNoteForm = function() {
+        $scope.noteFormVisible=false;
+      };
 
       $scope.getUserById = function(userId){
         return Meteor.users.findOne(userId);
